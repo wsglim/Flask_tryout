824827bb589b168b96d382c646a43000051e2266
Wouter Glimmerveen <wouterglimmerveen@live.nl>
2023-07-19 12:08:54 +0200

    basic Flask code

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p>Hallo wereld! Dit is het huiswerk van hoofdstuk 10.</p>"
+    return "<p>Hallo wereld! Dit is het huiswerk van hoofdstuk 11.</p>"
 
 
 if __name__=='__main__':
